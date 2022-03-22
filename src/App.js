5db520f12692fb46709c22b27619596d0f1d236a
unknown <tamiloreolaniyi@gmail.com>
2022-03-22 21:34:45 +0100

    todos useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './app.css'
+import {useState} from 'react'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <input type="text" />
       <button>Add Todo</button>
+
+      <ul>
+        <li>Take the dog out for a walk</li>
+        <li>Take the rubbish out</li>
+      </ul>
     </div>
   );
 }
