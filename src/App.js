2521624ae9c26f7f205a63b55a9c28c615d6b5cd
unknown <tamiloreolaniyi@gmail.com>
2022-03-22 21:55:41 +0100

    input useState

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(['Take the dog out for a walk','Take the rubbish out',"building amazing things"])
+  const [input,setInput]= useState('')
   return (
     <div className="App">
-      <input type="text" />
+      <input 
+      value={input}
+       onChange={event =>setInput(event.target.value)}/>
       <button>Add Todo</button>
 
       <ul>
-        <li>Take the dog out for a walk</li>
-        <li>Take the rubbish out</li>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
       </ul>
     </div>
   );
