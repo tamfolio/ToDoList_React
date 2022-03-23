87f9809fdbe4a22003b3931b0bf4dc4872294ab5
unknown <tamiloreolaniyi@gmail.com>
2022-03-23 14:07:25 +0100

    material UI for list item

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+// import './Todo.style.css'
+
 
 function Todo({todo}) {
   return (
-    <div>
-        <li>{todo}</li>
-    </div>
+    <List className='todo_list' >
+       <ListItem>
+           <ListItemText primary={todo} secondary='Dummy Lorem'/>
+       </ListItem>
+    </List>
   )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBmDegwI-VO4tZu2rB4dRCfzD3UzK1pLfg",
+
+    authDomain: "todo-app-8a1fd.firebaseapp.com",
+
+    projectId: "todo-app-8a1fd",
+
+    storageBucket: "todo-app-8a1fd.appspot.com",
+
+    messagingSenderId: "66143734225",
+
+    appId: "1:66143734225:web:e85a0f443065c6d9b178f0",
+
+    measurementId: "G-G62GQZN690"
+
+};
