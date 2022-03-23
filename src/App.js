b305b9450feb504eceb0ce2f7fa01ed3443f9f78
unknown <tamiloreolaniyi@gmail.com>
2022-03-23 12:13:02 +0100

    todo functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css'
 function App() {
   const [todos, setTodos] = useState(['Take the dog out for a walk','Take the rubbish out',"building amazing things"])
   const [input,setInput]= useState('')
+  console.log(input)
+
+  const addTodo = (event) => {
+    console.log('i am working')
+    setTodos([...todos, input])
+  }
   return (
     <div className="App">
-      <input 
-      value={input}
-       onChange={event =>setInput(event.target.value)}/>
-      <button>Add Todo</button>
+      <form>
+        <input 
+        value={input}
+        onChange={event =>setInput(event.target.value)}/>
+        <button onClick={addTodo}>Add Todo</button>
+      </form>
+      
 
       <ul>
         {todos.map(todo => (
