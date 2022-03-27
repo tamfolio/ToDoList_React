812cd7c4d8e0b4b7f5edd73070c51b100e40e2d3
unknown <tamiloreolaniyi@gmail.com>
2022-03-27 17:51:32 +0100

    firebase firestore

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css'
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Todo from './components/Todo';
+import db from './firebase'
+import firebase from 'firebase'
+ 
+
 
 function App() {
-  const [todos, setTodos] = useState(['Take the dog out for a walk','Take the rubbish out',"building amazing things"])
-  const [input,setInput]= useState('')
-  console.log(input)
+  const [todos, setTodos] = useState([]);
+  const [input,setInput]= useState('');
+
+useEffect(() => {
+  db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
+    // console.log(snapshot.docs.map(doc => doc.data()))
+    setTodos(snapshot.docs.map(doc => doc.data().todo))
+  })
+},[])
 
   const addTodo = (event) => {
     event.preventDefault();
-    console.log('i am working')
+    db.collection('todos').add({
+      todo: input,
+      timestamp:  firebase.firestore.FieldValue.serverTimestamp()
+    })
     setTodos([...todos, input])
     setInput('');
   }
   return (
     <div className="App">
+      
       <form>
          <TextField  label="Write a ToDo" variant="standard"
          value={input} 
