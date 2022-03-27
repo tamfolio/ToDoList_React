e3b390b6b7cdaca803a874e7289f8cb75e9354cb
unknown <tamiloreolaniyi@gmail.com>
2022-03-27 19:18:56 +0100

    Delete functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,15 +4,17 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 // import './Todo.style.css'
 import Button from '@mui/material/Button';
+import db from '../firebase'
 
 
-function Todo({todo}) {
+function Todo(props) {
   return (
     <List className='todo_list' >
        <ListItem>
-           <ListItemText primary={todo} secondary='Dummy Lorem'/>
+           <ListItemText primary={props.todo.todo} secondary='Dummy Lorem'/>
        </ListItem>
-       <Button>DELETE ME</Button>
+       <Button onClick={event => 
+         db.collection('todos').doc(props.todo.id).delete()}>DELETE ME</Button>
     </List>
   )
 }
