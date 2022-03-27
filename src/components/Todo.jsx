6ec4562ab86d225ee58bfdc1491d615b5e8202ca
unknown <tamiloreolaniyi@gmail.com>
2022-03-27 22:49:21 +0100

    edit modal

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 // import './Todo.style.css'
-import Button from '@mui/material/Button';
 import db from '../firebase'
+import Button from '@mui/material/Button';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 
 function Todo(props) {
+  const[open,setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  
   return (
+    <>
+    <Modal
+      open={open}
+      onClose={e => setOpen(false)}
+    >
+      <div>
+        <h1>Open</h1>
+      </div>
+    </Modal>
     <List className='todo_list' >
        <ListItem>
            <ListItemText primary={props.todo.todo} secondary='Dummy Lorem'/>
        </ListItem>
-       <Button onClick={event => 
-         db.collection('todos').doc(props.todo.id).delete()}>DELETE ME</Button>
+       <Button onClick={e => setOpen(true)}>Edit Me</Button>
+       <DeleteForeverIcon  onClick={event => 
+         db.collection('todos').doc(props.todo.id).delete()}/>
     </List>
+    </>
   )
 }
 
