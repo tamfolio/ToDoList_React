d177133e75e57e3a9f22237c54888a76ca81cf7b
unknown <tamiloreolaniyi@gmail.com>
2022-03-28 12:51:38 +0100

    project complete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -37,7 +37,7 @@ function Todo(props) {
       <div>
         <h1>I am a modal</h1>
          <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)}/>
-        <Button onClick={e => setOpen(false)}>Update Todo</Button>
+        <Button onClick={updateTodo}>Update Todo</Button>
       </div>
     </Modal>
     <List className='todo_list' >
